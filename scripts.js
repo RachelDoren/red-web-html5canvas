3eb43e83b5ecfaabde7b15c59789fd9372264306
RachelDoren <racheledoren@gmail.com>
2018-03-18 19:31:30 -0600

    Create second circle with object and methods
     Changes to be committed:
    	modified:   scripts.js

--- scripts.js
+++ scripts.js
@@ -38,11 +38,36 @@ var c = canvas.getContext("2d");
 //  c.stroke();
 // }
 
-function Circle(x, y) {
+function Circle(x, y, dx, dy, radius) {
     this.x = x;
     this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+
+    this.draw = function() {
+    c.beginPath(); 
+    c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+    c.fill();
+    c.strokeStyle = "white";
+    c.stroke();
+        }
+
+    this.update = function() {
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+        this.dx = -this.dx;
+    }
+    if (this.y + this.radius > innerHeight || this.x - this.radius < 0) {
+        this.dy = -this.dy
+    }
+    this.x+= this.dx
+    this.y+= this.dy;
+
+    this.draw();
+    }
 }
 
+var circle = new Circle(200,200, 3, 3, 70); //instantiate an object
 
 //recursive loop
 var x = Math.random() * innerWidth;
@@ -50,10 +75,11 @@ var y = Math.random() * innerHeight;
 var dx = .5; //velocity
 var dy = .5;
 var radius = 70
-var circleCount = 500
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
+
+    circle.update();
 
     c.beginPath(); 
     c.arc(x, y, radius, 0, 2 * Math.PI, false);
